ee7ab6480eda19ef78ce9df1de9a21775ce07ffb
ArtyomLobanov <lobanov98@yandex.ru>
2019-03-23 08:43:49 +0300

    Fix formatting

--- 1/homework/noisifier.py
+++ 1/homework/noisifier.py
@@ -1,18 +1,13 @@
 #!/usr/bin/python
-import numpy as np 
-import os 
-import sys
-import pandas as pd
-import numpy as np
-import random
-import itertools
-import librosa
-import IPython.display as ipd
-import matplotlib.pyplot as plt
-import random
-import ntpath
 import math
+import os
+import random
+import sys
+
+import librosa
+import numpy as np
 import soundfile as sf
+
 
 def get_sounds(path, sr):
     res = []
@@ -21,7 +16,7 @@ def get_sounds(path, sr):
             data, _ = librosa.core.load(os.path.join(path, file), sr)
             res.append(data)
     return res
-    
+
 
 def add_music(data, music, alpha):
     p = 0
@@ -32,22 +27,27 @@ def add_music(data, music, alpha):
         data[p:p + r] += alpha * music[:r]
         p += m
     np.clip(data, -1, 1, out=data)
-        
+
+
 def add_beep(data, beep, time, alpha):
     r = min(len(beep), len(data) - time)
     data[time:time + r] += alpha * beep[:r]
     np.clip(data[time:time + r], -1, 1, out=data[time:time + r])
-    
-def add_random_noise_mono(data, musics, beeps, music_alpha, beep_alpha, beep_frequency, rate):
+
+
+def add_random_noise_mono(data, musics, beeps, music_alpha,
+                          beep_alpha, beep_frequency, rate):
     n = len(data)
     music = random.choice(musics)
     add_music(data, music, music_alpha)
     beep_count = int(math.ceil(n / rate * beep_frequency))
     for i in range(beep_count):
-        add_beep(data, random.choice(beeps), random.randint(0, n - 1), beep_alpha)
-        
-    
-def add_random_noise_stereo(data, musics, beeps, music_alpha, beep_alpha, beep_frequency, rate):
+        add_beep(data, random.choice(beeps), random.randint(0, n - 1),
+                 beep_alpha)
+
+
+def add_random_noise_stereo(data, musics, beeps, music_alpha,
+                            beep_alpha, beep_frequency, rate):
     n = len(data)
     music = random.choice(musics)
     add_music(data[:, 0], music, music_alpha)
@@ -59,12 +59,14 @@ def add_random_noise_stereo(data, musics, beeps, music_alpha, beep_alpha, beep_f
         add_beep(data[:, 0], beep, time, beep_alpha)
         add_beep(data[:, 1], beep, time, beep_alpha)
 
-def generate_noised_versions(data_path, target_path, noise_path, music_alpha, beep_alpha, beep_frequency):
+
+def generate_noised_versions(data_path, target_path, noise_path, music_alpha,
+                             beep_alpha, beep_frequency):
     os.makedirs(target_path, exist_ok=True)
-    
+
     musics_cache = {}
     beeps_cache = {}
-    
+
     def get_noise_for_rate(rate):
         if rate in musics_cache:
             return musics_cache[rate], beeps_cache[rate]
@@ -73,27 +75,33 @@ def generate_noised_versions(data_path, target_path, noise_path, music_alpha, be
         musics_cache[rate] = musics
         beeps_cache[rate] = beeps
         return musics, beeps
-    
+
     for file in os.listdir(data_path):
         if file.endswith("wav"):
             data, rate = librosa.core.load(os.path.join(data_path, file))
             musics, beeps = get_noise_for_rate(rate)
-            add_random_noise_mono(data, musics, beeps, music_alpha, beep_alpha, beep_frequency, rate)
-            librosa.output.write_wav(os.path.join(target_path, file), data, rate)
+            add_random_noise_mono(data, musics, beeps, music_alpha,
+                                  beep_alpha, beep_frequency, rate)
+            librosa.output.write_wav(os.path.join(target_path, file),
+                                     data, rate)
         if file.endswith("flac"):
             data, rate = sf.read(os.path.join(data_path, file))
             musics, beeps = get_noise_for_rate(rate)
-            add_random_noise_stereo(data, musics, beeps, music_alpha, beep_alpha, beep_frequency, rate)
+            add_random_noise_stereo(data, musics, beeps, music_alpha,
+                                    beep_alpha, beep_frequency, rate)
             sf.write(os.path.join(target_path, file), data, rate)
 
+
 def main(argv):
-   data_path = argv[0]
-   target_path = argv[1]
-   noise_path = argv[2]
-   music_alpha = float(argv[3])
-   beep_alpha = float(argv[4])
-   beep_frequency = float(argv[5])
-   generate_noised_versions(data_path, target_path, noise_path, music_alpha, beep_alpha, beep_frequency)
+    data_path = argv[0]
+    target_path = argv[1]
+    noise_path = argv[2]
+    music_alpha = float(argv[3])
+    beep_alpha = float(argv[4])
+    beep_frequency = float(argv[5])
+    generate_noised_versions(data_path, target_path, noise_path,
+                             music_alpha, beep_alpha, beep_frequency)
+
 
 if __name__ == "__main__":
-   main(sys.argv[1:])
+    main(sys.argv[1:])
